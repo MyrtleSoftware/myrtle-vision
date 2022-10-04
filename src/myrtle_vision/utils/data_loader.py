c913dc2f431d236c1cafc7066220d28f6babc7ed
Aiken Cairncross <aiken@myrtle.ai>
2022-10-04 23:04:46 +0100

    Add RandomResizedCrop aug for segmentation

--- src/myrtle_vision/utils/data_loader.py
+++ src/myrtle_vision/utils/data_loader.py
@@ -1,3 +1,5 @@
+import math
+import numbers
 import os
 import random
 
@@ -123,6 +125,9 @@ class DlrsdLoader(torch.utils.data.Dataset):
         transform_list = []
         if "Resize" in transform_config:
             transform_list.append(ResizeBoth(transform_config["Resize"]))
+        if "RandomResizedCrop" in transform_config:
+            crop_size = transform_config["RandomResizedCrop"]
+            transform_list.append(RandomResizedCropBoth(crop_size))
         if "RandomHorizontalFlip" in transform_config:
             transform_list.append(RandomHorizontalFlipBoth())
 
@@ -189,3 +194,70 @@ class RandomHorizontalFlipBoth:
         else:
             return sample
 
+class RandomResizedCropBoth:
+    "Copied and modified from `torchvision.transforms.RandomResizedCrop`."
+
+    def __init__(
+            self,
+            size,
+            scale=(0.5, 1.0),
+            ratio=(3.0 / 4.0, 4.0 / 3.0),
+            interpolation=transforms.InterpolationMode.BILINEAR,
+    ):
+        self.size = self._setup_size(size, error_msg="Please provide only two dimensions (h, w) for size.")
+
+        self.interpolation = interpolation
+        self.scale = scale
+        self.ratio = ratio
+
+    @staticmethod
+    def _setup_size(size, error_msg):
+        if isinstance(size, numbers.Number):
+            return int(size), int(size)
+
+        if isinstance(size, Sequence) and len(size) == 1:
+            return size[0], size[0]
+
+        if len(size) != 2:
+            raise ValueError(error_msg)
+
+        return size
+
+    def get_params(self, image):
+        height, width = TF.get_image_size(image)
+        area = height * width
+
+        log_ratio = torch.log(torch.tensor(self.ratio))
+        for _ in range(10):
+            target_area = area * torch.empty(1).uniform_(self.scale[0], self.scale[1]).item()
+            aspect_ratio = torch.exp(torch.empty(1).uniform_(log_ratio[0], log_ratio[1])).item()
+
+            w = int(round(math.sqrt(target_area * aspect_ratio)))
+            h = int(round(math.sqrt(target_area / aspect_ratio)))
+
+            if 0 < w <= width and 0 < h <= height:
+                i = torch.randint(0, height - h + 1, size=(1,)).item()
+                j = torch.randint(0, width - w + 1, size=(1,)).item()
+                return i, j, h, w
+
+        # Fallback to central crop
+        in_ratio = float(width) / float(height)
+        if in_ratio < min(self.ratio):
+            w = width
+            h = int(round(w / min(self.ratio)))
+        elif in_ratio > max(self.ratio):
+            h = height
+            w = int(round(h * max(self.ratio)))
+        else:  # whole image
+            w = width
+            h = height
+        i = (height - h) // 2
+        j = (width - w) // 2
+        return i, j, h, w
+
+    def __call__(self, sample):
+        image, segmap = sample
+        i, j, h, w = self.get_params(image)
+        image = TF.resized_crop(image, i, j, h, w, self.size, self.interpolation)
+        segmap = TF.resized_crop(segmap, i, j, h, w, self.size, transforms.InterpolationMode.NEAREST)
+        return image, segmap
